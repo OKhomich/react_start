ee7e0a3dfa3e6a8465e501439a4d95fa09a86078
OKhomich <olga.khomich13@gmail.com>
2021-05-11 16:53:37 +0300

    lesson 10

---
+++ src/Technologies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Technologies() {
+    return (
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+        </ul>
+    );
+}
+
+export default Technologies;
